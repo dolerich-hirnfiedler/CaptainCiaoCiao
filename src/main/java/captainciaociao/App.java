b0a32fb3da2e97e22087faef37346aee1d40dfd7
Jakob Fassunge <dolerich_hirnfielder@MacBook-Air-von-Jakob.local>
2022-06-01 17:08:22 +0200

    initial commit

---
+++ src/main/java/captainciaociao/App.java
@@ -0,0 +1,13 @@
+package captainciaociao;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/captainciaociao/kapitel02/variablenunddatentypen/aufgabe2_3_2/Loot.java
@@ -0,0 +1,7 @@
+package captainciaociao.kapitel02.variablenunddatentypen.aufgabe2_3_2;
+
+import java.util.ArrayList;
+
+public class Loot {
+    private ArrayList<Bottle> bottles = new ArrayList<Bottle>();
+}
