0d4802dfb7ed471d07dc0583864b5396aecac6fb
Jakob Fassunge <dolerich_hirnfielder@MacBook-Air-von-Jakob.local>
2022-06-16 11:04:25 +0200

    next commit

--- src/main/java/captainciaociao/kapitel_4/_2_eindimensionale_arrays/aufgabe_7/Mountain.java
+++ src/main/java/captainciaociao/kapitel_4/_2_eindimensionale_arrays/aufgabe_7/Mountain.java
@@ -1,50 +1,45 @@
 package captainciaociao.kapitel_4._2_eindimensionale_arrays.aufgabe_7;
 
+import javax.swing.*;
+
 public class Mountain {
-    private int[] altitudes;
+  private int[] altitudes;
 
-    public Mountain(int[] altitudes) {
-        this.altitudes = altitudes;
+  public Mountain(int[] altitudes) { this.altitudes = altitudes; }
+
+  public int[] getAltitudes() { return altitudes; }
+
+  public void setAltitudes(int[] altitudes) { this.altitudes = altitudes; }
+
+  private int getHight() {
+    int height = 0;
+    for (int i = 0; i < this.altitudes.length; i++) {
+      if (height < this.altitudes[i])
+        height = this.altitudes[i];
     }
+    return height;
+  }
 
-    public int[] getAltitudes() {
-        return altitudes;
+  public void print() {
+    int[][] raster = new int[getHight() + 1][];
+    for (int i = 0; i < raster.length; i++) {
+      raster[i] = new int[this.altitudes.length];
+      for (int j = 0; j < raster[i].length; j++) {
+        raster[i][j] = -1;
+      }
     }
-
-    public void setAltitudes(int[] altitudes) {
-        this.altitudes = altitudes;
+    for (int i = 0; i < this.altitudes.length; i++) {
+      raster[raster.length - 1 - this.altitudes[i]][i] = this.altitudes[i];
     }
-
-    private int getHight() {
-        int height = 0;
-        for (int i = 0; i < this.altitudes.length; i++) {
-            if (height < this.altitudes[i])
-                height = this.altitudes[i];
+    for (int i = 0; i < raster.length; i++) {
+      for (int j = 0; j < raster[i].length; j++) {
+        if (raster[i][j] != -1) {
+          System.out.printf("*");
+        } else {
+          System.out.printf(" ");
         }
-        return height;
+      }
+      System.out.printf("%n");
     }
-
-    public void print() {
-        int[][] raster = new int[getHight() + 1][];
-        for (int i = 0; i < raster.length; i++) {
-            raster[i] = new int[this.altitudes.length];
-            for (int j = 0; j < raster[i].length; j++) {
-                raster[i][j] = -1;
-
-            }
-        }
-        for (int i = 0; i < this.altitudes.length; i++) {
-            raster[raster.length-1 - this.altitudes[i]][i] = this.altitudes[i];
-        }
-        for (int i = 0; i < raster.length; i++) {
-            for (int j = 0; j < raster[i].length; j++) {
-                if (raster[i][j] != -1) {
-                    System.out.printf("*");
-                }else{
-                    System.out.printf(" ");
-                }
-            }
-            System.out.printf("%n");
-        }
-    }
+  }
 }
--- src/main/java/captainciaociao/kapitel_4/_2_eindimensionale_arrays/aufgabe_7/MountainFactory.java
+++ src/main/java/captainciaociao/kapitel_4/_2_eindimensionale_arrays/aufgabe_7/MountainFactory.java
@@ -1,36 +1,1 @@
-package captainciaociao.kapitel_4._2_eindimensionale_arrays.aufgabe_7;
-
-import java.util.Random;
-
-public class MountainFactory {
-    private static final Random RANDOM = new Random();
-
-    private static int[] generateAltitude(int height) {
-        int[] constructionInformation = new int[2 * height + 1];
-        int size = 0;
-        int j = 0;
-        int index = 0;
-        for (int i = 0; i < constructionInformation.length; i++) {
-            constructionInformation[i] = RANDOM.nextInt(2) + 1;
-            size += constructionInformation[i];
-        }
-
-        int[] altitude = new int[size];
-        for (int i = 0; i < constructionInformation.length; i++) {
-            j = 0;
-            for (j = 0; j < constructionInformation[i]; j++) {
-                if (i <= height) {
-                    altitude[index] = i;
-                } else {
-                    altitude[index] = constructionInformation.length-1 - i;
-                }
-                index++;
-            }
-        }
-        return altitude;
-    }
-
-    public static Mountain initMountain(int height) {
-        return new Mountain(generateAltitude(height));
-    }
-}
+package captainciaociao.kapitel_4._2_eindimensionale_arrays.aufgabe_7;import java.util.Random;public class MountainFactory {  private static final Random RANDOM = new Random();  private static int[] generateAltitude(int height) {    int[] constructionInformation = new int[2 * height + 1];    int size = 0;    int j = 0;    int index = 0;    for (int i = 0; i < constructionInformation.length; i++) {      constructionInformation[i] = RANDOM.nextInt(2) + 1;      size += constructionInformation[i];    }    int[] altitude = new int[size];    for (int i = 0; i < constructionInformation.length; i++) {      j = 0;      for (j = 0; j < constructionInformation[i]; j++) {        if (i <= height) {          altitude[index] = i;        } else {          altitude[index] = constructionInformation.length - 1 - i;        }        index++;      }    }    return altitude;  }  public static Mountain initMountain(int height) {    return new Mountain(generateAltitude(height));  }}
