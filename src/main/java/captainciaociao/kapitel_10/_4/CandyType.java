9e529b732f10dd01fdb87cd96533e2a2248e4447
Jakob Fassunge <jakob.fassunge1998@gmail.com>
2022-07-26 18:08:15 +0200

    added two variants of the random method

--- src/main/java/captainciaociao/kapitel_10/_4/CandyType.java
+++ src/main/java/captainciaociao/kapitel_10/_4/CandyType.java
@@ -13,4 +13,31 @@ public enum CandyType {
             return Optional.empty();
         }
     }
+
+    public static CandyType random() {
+        return values()[(int) (Math.random() * values().length)];
+        // int random = (int) (Math.random() * 7);
+        // switch (random) {
+        // case 0:
+        // return CARAMELS;
+        // case 1:
+        // return CHOCOLATE;
+        // case 2:
+        // return GUMMIES;
+        // case 3:
+        // return LICORICE;
+        // case 4:
+        // return LOLLIPOPS;
+        // case 5:
+        // return CHEWING_GUMS;
+        // case 6:
+        // return COTTON_CANDY;
+        // }
+        // return null;
+
+    }
+
+    public static void main(String[] args) {
+        System.out.print(CandyType.random());
+    }
 }
