a19aab9d97522dd002725f01d417694bb0d9683d
Jakob Fassunge <dolerich_hirnfielder@MacBook-Air-von-Jakob.local>
2022-08-04 13:34:11 +0200

    added ascii

---
+++ src/main/java/captainciaociao/kapitel_11/_1/Ascii.java
@@ -0,0 +1,14 @@
+package captainciaociao.kapitel_11._1;
+
+public class Ascii {
+  public static void main(String[] args) {
+    int min = 32;
+    int max = 127;
+    int rows = 16;
+    String header = String.format("%s%s", "Dec", "Hex");
+    for (int i = min; i < max; i++) {
+      for (int j = 0; j < min; j++) {
+      }
+    }
+  }
+}
