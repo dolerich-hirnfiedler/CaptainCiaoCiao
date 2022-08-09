e1eb31515604f3fe3e673dbe1b9cb2364d484f6c
Jakob Fassunge <jakob.fassunge1998@gmail.com>
2022-08-09 13:27:58 +0200

    worked on 11.1

--- src/main/java/captainciaociao/kapitel_11/_1/Ascii.java
+++ src/main/java/captainciaociao/kapitel_11/_1/Ascii.java
@@ -6,9 +6,13 @@ public class Ascii {
     int max = 127;
     int rows = 16;
     String header = String.format("%s%s", "Dec", "Hex");
-    for (int i = min; i < max; i++) {
-      for (int j = 0; j < min; j++) {
-      }
+    for (int i = min; i < rows + min; i++) {
+      // System.out.printf("%d %X %c %n", i, i, i);
     }
+    PrintingTable table = new PrintingTable(17, 3);
+    table.setHeader(0, "Dec");
+    table.setHeader(1, "Hex");
+    table.setHeader(2, "Sym");
+    table.printTable();
   }
 }
---
+++ src/main/java/captainciaociao/kapitel_11/_1/PrintingTable.java
@@ -0,0 +1,26 @@
+package captainciaociao.kapitel_11._1;
+
+public class PrintingTable {
+    private String[][] table;
+
+    public PrintingTable(int rows, int columns) {
+        this.table = new String[rows][columns];
+    }
+
+    public void setHeader(int column, String value) {
+        this.table[0][column] = value;
+    }
+
+    public void printTable() {
+        for (String[] row : this.table) {
+            System.out.printf("%n");
+            for (String value : row) {
+                System.out.printf("%s ", value);
+            }
+        }
+    }
+
+    public void setValue(int row, int column, String value) {
+
+    }
+}
