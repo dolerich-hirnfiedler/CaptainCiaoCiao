480626062fae3e82235bb07bc1297ce08e1fea16
Jakob Fassunge <dolerich_hirnfielder@MacBook-Air-von-Jakob.local>
2022-08-11 17:08:14 +0200

    started with 11.2.5

---
+++ src/main/java/captainciaociao/kapitel_11/_2/NumbersToStringsConverter.java
@@ -0,0 +1,8 @@
+package captainciaociao.kapitel_11._2;
+
+public class NumbersToStringsConverter {
+    public static void main(String[] args) {
+        StringBuffer example = new StringBuffer("99 bottles of beer make CaptainCiaoCiao happy for 10 years");
+
+    }
+}
