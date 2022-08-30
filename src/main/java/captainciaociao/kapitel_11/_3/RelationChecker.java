aaaf9298eea3448d04f5d33f6b5908aefe769a4e
Jakob Fassunge <jf@Jakobs-MacBook-Air.local>
2022-08-30 16:30:36 +0200

    finished 11.3.3

--- src/main/java/captainciaociao/kapitel_11/_3/RelationChecker.java
+++ src/main/java/captainciaociao/kapitel_11/_3/RelationChecker.java
@@ -1,5 +1,8 @@
 package captainciaociao.kapitel_11._3;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class RelationChecker {
     private static boolean check(Integer first, Integer second, String relation) {
         if (relation.matches("<")) {
@@ -17,14 +20,34 @@ public class RelationChecker {
 
     public static boolean checkRelation(String input) {
         String[] numbers = input.split("<|>|=");
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = numbers[i].trim();
+        }
+        ArrayList<String> numbersCleaned = new ArrayList<String>();
+        for (int i = 0; i < numbers.length; i++) {
+            if (!numbers[i].matches("")) {
+                numbersCleaned.add(numbers[i]);
+            }
+        }
         String[] relations = input.split("[0-9]+");
-        if (numbers.length <= relations.length) {
+        for (int i = 0; i < relations.length; i++) {
+            relations[i] = relations[i].trim();
+        }
+        ArrayList<String> relationsCleaned = new ArrayList<>();
+        for (int i = 0; i < relations.length; i++) {
+            if (!relations[i].matches("")) {
+                relationsCleaned.add(relations[i]);
+            }
+        }
+
+
+        if (numbersCleaned.size() <= relationsCleaned.size()) {
             return false;
         }
         boolean isValid = true;
-        for (int i = 0; i < numbers.length - 1; i++) {
-            if (!check(Integer.getInteger(numbers[i]), Integer.getInteger(numbers[i + 1]),
-                    relations[i])) {
+        for (int i = 0; i < numbersCleaned.size() - 1; i++) {
+            if (!check(Integer.parseInt(numbersCleaned.get(i)),
+                    Integer.parseInt(numbersCleaned.get(i + 1)), relationsCleaned.get(i))) {
                 isValid = false;
             }
         }
