4b281451c6099671c5a39348023d8ca2a093f09b
Jakob Fassunge <jf@Jakobs-MacBook-Air.local>
2022-08-30 18:44:50 +0200

    11.3.4 -- done

--- src/main/java/captainciaociao/kapitel_11/_3/A1Notation.java
+++ src/main/java/captainciaociao/kapitel_11/_3/A1Notation.java
@@ -1,7 +1,26 @@
 package captainciaociao.kapitel_11._3;
 
 public class A1Notation {
+    public static String printResult(int[] res) {
+        String result = "[";
+        for (int i = 0; i < res.length; i++) {
+            result += " " + res[i] + ",";
+        }
+        result += "]";
+        return result;
+    }
+
     private static int parseLetters(String input) {
+        int returnValue = 0;
+        if (input.matches("Z+")) {
+            returnValue = (int) Math.pow((double) 26, (double) input.length());
+        } else {
+            returnValue = input.charAt(input.length() - 1) - 64;
+            for (int i = 0; i < input.length() - 1; i++) {
+                returnValue += (input.charAt(i) - 64) * Math.pow(26, input.length() - i - 1);
+            }
+        }
+        return returnValue;
 
     }
 
@@ -18,6 +37,12 @@ public class A1Notation {
     }
 
     public static void main(String[] args) {
-
+        System.out.println(printResult(parseA1Notation("A1")));
+        System.out.println(printResult(parseA1Notation("Z2")));
+        System.out.println(printResult(parseA1Notation("AA34")));
+        System.out.println(printResult(parseA1Notation("BZ")));
+        System.out.println(printResult(parseA1Notation("34")));
+        System.out.println(printResult(parseA1Notation(" ")));
+        System.out.println(printResult(parseA1Notation("")));
     }
 }
