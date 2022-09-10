434f455e164d09f497b8f9715a9f10309f451ffa
Jakob Fassunge <jf@Jakobs-MacBook-Air.local>
2022-09-10 10:17:37 +0200

    begun 11.3.6

--- src/main/java/captainciaociao/kapitel_11/_3/SimpleStringCompressor.java
+++ src/main/java/captainciaociao/kapitel_11/_3/SimpleStringCompressor.java
@@ -1,8 +1,39 @@
 package captainciaociao.kapitel_11._3;
 
 import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class SimpleStringCompressor {
+  public static String advancedCompress(String input) {
+    String output = "";
+    Pattern numbers = Pattern.compile("[0-9]");
+    Matcher numberMatcher = numbers.matcher(input);
+    int counter = 1;
+    if (!numberMatcher.find()) {
+      // System.out.printf("%d%n", input.length());
+      for (int i = 1; i < input.length(); i++) {
+        if (input.charAt(i) == input.charAt(i - 1)) {
+          counter++;
+        } else {
+          if (counter != 1) {
+            output += input.substring(i - 1, i);
+            output += counter;
+          } else {
+            output += input.substring(i - 1, i);
+          }
+          counter = 1;
+        }
+      }
+      output += input.substring(input.length() - 1, input.length());
+    } else {
+
+      return output;
+    }
+
+    return output;
+  }
+
   public static String compress(String input) {
     String output = "";
     boolean isDotFirst = input.charAt(0) == '.';
@@ -35,37 +66,28 @@ public class SimpleStringCompressor {
         } else {
           output += String.format("%d.", dotsCleaned.get((int) i / 2).length());
         }
-
       }
     }
 
-    // First ide have a better one now
-    // int counter = 1;
-    // if (input.matches("[.-]+")) {
-    // for (int i = 1; i < input.length(); i++) {
-    // if (input.charAt(i) == input.charAt(i - 1)) {
-    // counter++;
-    // } else {
-    // if (counter != 1) {
-    // output += input.substring(i - 1, i);
-    // output += counter;
-    // } else {
-    // output += input.substring(i - 1, i);
-    // }
-    // counter = 1;
-    // }
-    // }
-
-    // }
     return output;
   }
 
   public static String decompress(String input) {
-    return "";
+    String output = "";
+
+    Pattern numbers = Pattern.compile("[0-9]");
+    Matcher numberMatcher = numbers.matcher(input);
+
+    return output;
   }
 
   public static void main(String[] args) {
     String test = "--....--------..--";
+    String secodTest = "sakjdfhassdsssswoehegkjaökssssssdfddffffffeeeeewpwwwwwj";
+    String thirdTest = "9sakjdfhassdsssswoehegkjaökssssssdfddffffffeeeeewpwwwwwj";
     System.out.printf("%s%n", compress(test));
+    System.out.printf("%s%n", advancedCompress(test));
+    System.out.printf("%s%n", advancedCompress(secodTest));
+    System.out.printf("%s%n", advancedCompress(thirdTest));
   }
 }
