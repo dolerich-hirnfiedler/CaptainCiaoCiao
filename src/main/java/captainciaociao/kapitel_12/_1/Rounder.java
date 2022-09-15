403394f779a426fbeeae828cbd7c4c3f0f69902e
Jakob Fassunge <jakob.fassunge1998@gmail.com>
2022-09-15 09:27:48 +0200

    done 12.1.1

--- src/main/java/captainciaociao/kapitel_12/_1/Rounder.java
+++ src/main/java/captainciaociao/kapitel_12/_1/Rounder.java
@@ -5,37 +5,21 @@ import java.util.Random;
 public class Rounder {
     private static final Random RANDOM = new Random();
 
-    private static RoundingMode randomReturn() {
-        RoundingMode returnValue = RoundingMode.UNKNOWN;
-        switch (RANDOM.nextInt(3)) {
-            case 0:
-                returnValue = RoundingMode.CAST;
-            case 1:
-                returnValue = RoundingMode.FLOOR;
-            case 2:
-                returnValue = RoundingMode.ROUND;
-            case 3:
-                returnValue = RoundingMode.RINT;
-        }
-        return returnValue;
-
+    private static RoundingMode detectRoundingMOde(int rounded, double number) {
+        return rounded == (int) number ? RoundingMode.CAST
+                : rounded == (int) Math.floor(number) ? RoundingMode.FLOOR
+                        : rounded == (int) Math.ceil(number) ? RoundingMode.CEIL
+                                : rounded == (int) Math.round(number) ? RoundingMode.ROUND
+                                        : rounded == (int) Math.rint(number) ? RoundingMode.RINT
+                                                : RoundingMode.UNKNOWN;
     }
 
     public static RoundingMode detectRoundingMode(int sum, double... numbers) {
-        double numbersSum = 0;
+        double realSum = 0;
         for (int i = 0; i < numbers.length; i++) {
-            numbersSum += numbers[i];
+            realSum += numbers[i];
         }
-        if (numbersSum >= 0) {
-            if (numbersSum - (int) numbersSum >= 0 && numbersSum - (int) numbersSum <= 0.5) {
-
-            } else {
-
-            }
-        } else {
-
-        }
-        return RoundingMode.UNKNOWN;
+        return detectRoundingMOde(sum, realSum);
     }
 
     public static void main(String[] args) {
