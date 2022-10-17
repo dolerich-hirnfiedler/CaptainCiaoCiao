b4d4307bf284d7f2541bdd3492aec0c0526d8305
Jakob Fassunge <jakob.fassunge@esentri.com>
2022-10-17 15:21:02 +0200

    finished 15.2.12

--- src/main/java/captainciaociao/kapitel_15/_2_listen/Ship.java
+++ src/main/java/captainciaociao/kapitel_15/_2_listen/Ship.java
@@ -30,5 +30,41 @@ public class Ship {
         ships.get(new Random().nextInt(ships.size())).addName("Covid Cough");
         int index = new Random().nextInt(ships.size());
         ListIterator<Ship> iterator = ships.listIterator(index);
+        if (iterator.nextIndex() >= ships.size() / 2) {
+            if (searchRight(iterator)) {
+                System.out.println("-> at ship" + iterator.previousIndex());
+            } else if (searchLeft(iterator)) {
+                System.out.println("-> <- at ship" + iterator.nextIndex());
+
+            } else {
+                System.out.println("Not found");
+            }
+        } else {
+            if (searchLeft(iterator)) {
+                System.out.println("<- at ship" + iterator.nextIndex());
+            } else if (searchRight(iterator)) {
+                System.out.println("<- -> at ship" + iterator.previousIndex());
+            } else {
+                System.out.println("Not found!");
+            }
+        }
+    }
+
+    private static boolean searchLeft(ListIterator<Ship> iterator) {
+        while (iterator.hasPrevious()) {
+            if (iterator.previous().contains("Covid Cough")) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static boolean searchRight(ListIterator<Ship> iterator) {
+        while (iterator.hasNext()) {
+            if (iterator.next().contains("Covid Cough")) {
+                return true;
+            }
+        }
+        return false;
     }
 }
