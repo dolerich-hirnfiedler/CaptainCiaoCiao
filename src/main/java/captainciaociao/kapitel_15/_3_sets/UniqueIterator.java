0bd523dfb3d17d52e090686969cfec2a9c48aacc
Jakob Fassunge <jakob.fassunge@esentri.com>
2022-10-18 14:36:45 +0200

    started 15.3.6

---
+++ src/main/java/captainciaociao/kapitel_15/_3_sets/UniqueIterator.java
@@ -0,0 +1,25 @@
+package captainciaociao.kapitel_15._3_sets;
+
+import java.util.Iterator;
+
+public class UniqueIterator<E> implements Iterator<E> {
+
+    /**
+     *
+     */
+    public UniqueIterator(Iterator<? extends E> iterator) {
+    }
+
+    @Override
+    public boolean hasNext() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public E next() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
