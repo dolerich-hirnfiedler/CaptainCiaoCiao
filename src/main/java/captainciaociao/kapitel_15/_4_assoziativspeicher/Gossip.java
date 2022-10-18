bf1aaaf8471855502af69e16c275a62c26b89b84
Jakob Fassunge <jakob.fassunge@esentri.com>
2022-10-18 16:57:00 +0200

    started 15.4.4

---
+++ src/main/java/captainciaociao/kapitel_15/_4_assoziativspeicher/Gossip.java
@@ -0,0 +1,17 @@
+package captainciaociao.kapitel_15._4_assoziativspeicher;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Gossip {
+    public static List<String> importantGossip(String... words) {
+        List<String> results = new ArrayList<>();
+
+        return results;
+    }
+
+    public static void main(String[] args) {
+        String[] words = { "Baby Shark", "Corona", "Baby Yoda", "Corona", "Tiger King", "Baby Yoda", "David Bowie",
+                "Corona", "Kylie Jenner", "Kardashian", "Love Island", "Bachelorette", "Billie Eilish", "Corona" };
+    }
+}
