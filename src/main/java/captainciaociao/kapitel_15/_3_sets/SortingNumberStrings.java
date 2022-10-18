2f02b302cee71e7d3b9fc8a656c441e91014dd32
Jakob Fassunge <jakob.fassunge@esentri.com>
2022-10-18 15:00:56 +0200

    done 15.3.6

--- src/main/java/captainciaociao/kapitel_15/_3_sets/SortingNumberStrings.java
+++ src/main/java/captainciaociao/kapitel_15/_3_sets/SortingNumberStrings.java
@@ -1,17 +1,24 @@
 package captainciaociao.kapitel_15._3_sets;
 
+import java.math.BigDecimal;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.LinkedHashSet;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class SortingNumberStrings {
-    public static final String[] EXAMPLE_STRINGS = { "-13.123", "0", "0", "10101010", "10101010.0", "0.0", "-0" };
+    public static final String[] EXAMPLE_STRINGS = { "-13.123", "0", "0", "10101010", "10101010.0", "0.0", "-0.0" };
+
+    static Comparator<String> comp = Comparator.comparing((String s) -> new BigDecimal(s))
+            .thenComparing(Comparator.naturalOrder());
 
     public static Collection<String> sort(List<String> numbers) {
-        Set<String> filteredNumbers = new LinkedHashSet<>(numbers);
+        Set<String> filteredNumbers = new TreeSet<>(comp);
+        filteredNumbers.addAll(numbers);
         return filteredNumbers;
     }
 
--- src/main/java/captainciaociao/kapitel_15/_3_sets/UniqueIterator.java
+++ src/main/java/captainciaociao/kapitel_15/_3_sets/UniqueIterator.java
@@ -1,25 +1,44 @@
 package captainciaociao.kapitel_15._3_sets;
 
+import java.util.HashSet;
 import java.util.Iterator;
+import java.util.Set;
 
 public class UniqueIterator<E> implements Iterator<E> {
+
+    private Iterator<? extends E> iterator;
+    private E next;
+    private final Set<E> hasSeenSet = new HashSet<>();
 
     /**
      *
      */
     public UniqueIterator(Iterator<? extends E> iterator) {
+        this.iterator = iterator;
+        next = lookahead();
+    }
+
+    private E lookahead() {
+        while (iterator.hasNext()) {
+            E next = iterator.next();
+            if (!hasSeenSet.contains(next)) {
+                return next;
+            }
+        }
+        return null;
     }
 
     @Override
     public boolean hasNext() {
-        // TODO Auto-generated method stub
-        return false;
+        return next != null;
     }
 
     @Override
     public E next() {
-        // TODO Auto-generated method stub
-        return null;
+        E result = next;
+        hasSeenSet.add(result);
+        next = lookahead();
+        return result;
     }
 
 }
