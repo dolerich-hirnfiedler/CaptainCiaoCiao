77ec943180c3b13c40c526d6c73a61522b3f3659
Jakob Fassunge <jakob.fassunge@esentri.com>
2022-10-19 14:06:42 +0200

    start 16.1.1

---
+++ src/main/java/captainciaociao/kapitel_16/_1_regular_streams_with_termianle_and_inter_ops/StreamingHeroes.java
@@ -0,0 +1,11 @@
+package captainciaociao.kapitel_16._1_regular_streams_with_termianle_and_inter_ops;
+
+public class StreamingHeroes {
+    // 16.1.1 Heldenepos: Stream-API kennen lernen
+
+
+
+    public static void main(String[] args) {
+
+    }
+}
