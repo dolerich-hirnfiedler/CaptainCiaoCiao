80b949a278226739045149ecae313b7b83d33e04
Jakob Fassunge <jakob.fassunge@esentri.com>
2022-10-19 14:19:57 +0200

    working 16.1.1

--- src/main/java/captainciaociao/kapitel_16/_1_regular_streams_with_termianle_and_inter_ops/StreamingHeroes.java
+++ src/main/java/captainciaociao/kapitel_16/_1_regular_streams_with_termianle_and_inter_ops/StreamingHeroes.java
@@ -1,11 +1,18 @@
 package captainciaociao.kapitel_16._1_regular_streams_with_termianle_and_inter_ops;
 
+import captainciaociao.kapitel_10._2_comperator_comparable.aufgabe_2.Heroes;
+import captainciaociao.kapitel_10._2_comperator_comparable.aufgabe_2.Heroes.Hero.Sex;
+
 public class StreamingHeroes {
     // 16.1.1 Heldenepos: Stream-API kennen lernen
 
 
 
     public static void main(String[] args) {
-
+        Heroes.ALL.stream()
+                .forEach(h -> System.out.printf("%s,%s,%s%n", h.name, h.sex, h.yearFirstApperance));
+        System.out.println(Heroes.ALL.stream().allMatch(h -> h.yearFirstApperance >= 1900));
+        System.out.println(Heroes.ALL.stream()
+                .anyMatch(h -> h.sex.equals(Sex.FEMALE) && h.yearFirstApperance >= 1950));
     }
 }
